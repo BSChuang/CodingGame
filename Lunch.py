40d948594c4dbfc50b2faec7b36d69390237ac96
BuildTools <unconfigured@null.spigotmc.org>
2016-11-05 22:41:14 -0400

    Calculate where to go for whole week

--- Lunch.py
+++ Lunch.py
@@ -2,5 +2,8 @@ import random
 prevEatery = 'Golden Dragon'
 
 foods = ['Bagel Place', 'Golden Dragon', "Sam's", 'Diner', 'Dawg House']
-foods.remove(prevEatery)
-print(foods[random.randint(0, len(foods) - 1)])
+#foods.remove(prevEatery)
+for i in range(len(foods)):
+    whereToGo = foods[random.randint(0, len(foods) - 1)] 
+    print(whereToGo)
+    foods.remove(whereToGo)
