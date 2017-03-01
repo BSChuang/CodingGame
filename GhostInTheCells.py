551e4882af4d6682d127769ba52d510a507305dd
BuildTools <ben10chuang@gmail.com>
2017-02-28 20:52:44 -0500

    Offense

--- GhostInTheCells.py
+++ GhostInTheCells.py
@@ -1,16 +1,3 @@
-'''facts = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Factories
-cybsToSend = 10
-moveText = ""
-def Move(source, destination, cybs):
-    return "MOVE " +  str(source) + " " + str(destination) + " " + str(cybs) + "; "
-for i in range(len(facts)): # Sending enough cyborgs to necessary factory
-    if cybsToSend > facts[i][1]:
-        cybsToSend -= facts[i][1]
-        moveText += Move(1, 2, str(facts[i][1]))
-    else:
-        moveText += Move(1, 2, str(cybsToSend))
-print(moveText)'''
-
 import sys
 import math
 import random
@@ -21,7 +8,10 @@ for i in range(link_count):
     factory_1, factory_2, distance = [int(j) for j in input().split()]
 
 def Move(source, destination, cyborgCount):
-    return "MOVE" + " " + str(source) + " " + str(destination) + " " + str(cyborgCount) + ";"
+    return "MOVE" + " " + str(source) + " " + str(destination) + " " + str(round(cyborgCount)) + ";"
+
+def Bomb(source, destination):
+    return "BOMB" + " " + str(source) + " " + str(destination) + ";"
 
 turn = 0
 while True:
@@ -47,6 +37,8 @@ while True:
     neutFactProd = []
     
     oppFacts = []
+    oppFactCyb = []
+    oppFactProd = []
     
     factOwner = []
     factCyb = []
@@ -94,6 +86,8 @@ while True:
                 oppFacts.append(entity_id)
                 oppCyb += int(arg_2)
                 oppProd += int(arg_3)
+                oppFactCyb.append(int(arg_2))
+                oppFactProd.append(int(arg_3))
         elif entity_type == "TROOP":
             
             tempList.append(int(arg_1))
@@ -121,10 +115,13 @@ while True:
     
 
     # OFFENSE - Attack whatever they attack or claim neutrals. Have enough cyborgs to fend off an attack
-    if turn == 1:
-        maxNeutFact = neutFacts[neutFactProd.index(max(neutFactProd))] # Find highest factory production
-        startFact = ownFacts[0]
-        moveText += Move(startFact, maxNeutFact, ownFactCyb[0])#facts[maxNeutFact][1] + 1)
+    if turn == 3 or turn == 13:
+        maxOppFact = oppFacts[oppFactCyb.index(max(oppFactCyb))] # Find highest opp factory production
+        moveText += Bomb(ownFacts[0], maxOppFact)
+    if turn == 1 or turn == 4 or turn == 8:
+        maxNeutFact = neutFacts[neutFactProd.index(max(neutFactProd))] # Find highest neut factory production
+        ownCybFact = ownFacts[ownFactCyb.index(max(ownFactCyb))] # Find highest own factory cyborgs
+        moveText += Move(ownCybFact, maxNeutFact, max(ownFactCyb) / 2) # 
     #elif :
         
     moveText += "WAIT;"
@@ -133,176 +130,30 @@ while True:
     # DEFENSE - If factory is being attacked, send equal to help troops
     for i in range(len(ownFacts)):
         if facts[ownFacts[i]][0] == 1 and factCyb[ownFacts[i]] < 1: # If its gone negative and still own factory, send equal troops plus some more, 
-           print(str(facts[ownFacts[i]][0]) + " " + str(factCyb[ownFacts[i]]), file=sys.stderr)
-       # START HERE: SEND TROOP WHEREVER NEEDED
+            cybsToSend = abs(factCyb[ownFacts[i]]) + 3
+            for j in range(len(ownFacts)): # Sending enough cyborgs to necessary factory
+                if j != i: # If not the same factory
+                    if cybsToSend > facts[ownFacts[j]][1]: # If there are more cybs that need to be sent than cybs available, move all minus 2
+                        cybsToSend -= facts[ownFacts[j]][1]
+                        moveText += Move(ownFacts[j], ownFacts[i], facts[ownFacts[j]][1] - 2)
+                    else:
+                        moveText += Move(ownFacts[j], ownFacts[i], cybsToSend) # If there are enough cybs to send, send all necessary
+    #print(str(facts[ownFacts[i]][0]) + " " + str(factCyb[ownFacts[i]]), file=sys.stderr)
 
 
 
     print(moveText[:-1])
 
 
- 
-    '''if ownProd <= oppProd:
-        if stillNeut:
-            maxNeutFact = neutFacts[neutFactProd.index(max(neutFactProd))] # Find highest factory production
-        else:
-            max
-        maxOwnFact = ownFacts[ownFactCyb.index(max(ownFactCyb))] # Find the factory with the highest production
-        moveText += Move(maxOwnFact, maxNeutFact, abs(factCyb[maxNeutFact]) + 1) # Move one more cyborg than necessary
+'''facts = [[1, 2, 3], [4, 5, 6], [7, 8, 9]] # Factories
+cybsToSend = 10
+moveText = ""
+def Move(source, destination, cybs):
+    return "MOVE " +  str(source) + " " + str(destination) + " " + str(cybs) + "; "
+for i in range(len(facts)): # Sending enough cyborgs to necessary factory
+    if cybsToSend > facts[i][1]:
+        cybsToSend -= facts[i][1]
+        moveText += Move(1, 2, str(facts[i][1]))
     else:
-        moveText += "WAIT;"'''
-    
-
-    
-    
-    
-    
-    
-
-    
-    
-    '''
-import sys
-import math
-import random
-
-factory_count = int(input())  # the number of factories
-link_count = int(input())  # the number of links between factories
-for i in range(link_count):
-    factory_1, factory_2, distance = [int(j) for j in input().split()]
-
-def Move(source, destination, cyborgCount):
-    return "MOVE" + " " + str(source) + " " + str(destination) + " " + str(cyborgCount) + ";"
-
-turn = 0
-while True:
-    turn += 1
-    print(str(turn), file=sys.stderr)
-    entity_count = int(input())  # the number of entities (e.g. factories and troops)
-    ownedFact = [] # Factory Number
-    ownedFactCyb = [] # Factory Cyborgs. In order of owned Factory list
-    ownedFactProd = []
-    neutFact = []
-    neutFactCyb = []
-    factFutCyb = []
-    neutFactProd = []
-    oppFact = []
-    oppFactCyb = []
-    oppFactProd = []
-    
-    stillNeut = False;
-    stillOpp = False;
-    for i in range(entity_count):
-        entity_id, entity_type, arg_1, arg_2, arg_3, arg_4, arg_5 = input().split()
-        entity_id = int(entity_id)
-        if entity_type == "FACTORY":
-            owner = int(arg_1) # 1=mine,-1=opponent, 0=neutral
-            numCyb = int(arg_2) # Number of cyborgs in the factory
-            factProd = int(arg_3) # Factory Production (0-3)
-            if owner == 1:
-                ownedFact.append(entity_id)
-                ownedFactCyb.append(numCyb)
-            elif owner == 0 and factProd > 0:
-                neutFact.append(entity_id)
-                neutFactCyb.append(entity_id)
-                neutFactProd.append(factProd)
-                stillNeut = True
-            elif owner == -1:
-                oppFact.append(entity_id)
-                oppFactCyb.append(numCyb)
-                oppFactProd.append(factProd)
-                stillOpp = True
-        elif entity_type == "TROOP":
-            owner = int(arg_1) # 1=mine,-1=opponent
-            factDepart = int(arg_2) # Factory the troop left from
-            factTarget = int(arg_3) # Factory targetted by the troop
-            CybTroop = int(arg_4) # number of cyborgs in the troop (positive integer)
-            arrivalTurns = int(arg_5) # remaining number of turns before the troop arrives (positive integer)
-            #if factTarget in ownedFact: # If the target factory is mine, add the troops to it
-             #   ownedFactCyb[ownedFact.index(factTarget)] += CybTroop * owner
-            #if factTarget in neutFact:
-             #   neutFactCyb[neutFact.index(factTarget)] += CybTroop * owner
-            #if factTarget in oppFact:
-             #   oppFactCyb[oppFact.index(factTarget)] += CybTroop * owner
-    
-    if stillNeut:
-        moveText = ""
-        if turn == 10 or turn == 20:
-            startFact = ownedFact[random.randint(0, len(ownedFact) - 1)]
-            endFact = oppFact[oppFactProd.index(max(oppFactProd))]
-            moveText = "BOMB " + str(startFact) + " " + str(endFact) + ";"
-            
-        for i in range(len(ownedFact)):
-            startFact = ownedFact[i]
-            
-    
-    
-    
-    if stillNeut:
-        moveText = ""
-        if turn == 10 or turn == 20:
-            startFact = ownedFact[random.randint(0, len(ownedFact) - 1)]
-            endFact = oppFact[oppFactProd.index(max(oppFactProd))]
-            moveText = "BOMB " + str(startFact) + " " + str(endFact) + ";"
-
-        for i in range(len(ownedFact)):
-            startFact = ownedFact[i]
-            endFact = neutFact[neutFactProd.index(max(neutFactProd))] # Neutral factory with highest production
-            moveText += Move(startFact, endFact, int(round(ownedFactCyb[i] / 4)))# int(round(ownedFactCyb[i] / 4))) # Sets one move to factory
-            if len(neutFact) > 2:
-                neutFactProd.pop(neutFact.index(endFact)) # Removes previous factory production from list
-                neutFact.remove(endFact) # Removes previous factory from list
-                endFact = neutFact[neutFactProd.index(max(neutFactProd))] # Second highest factory from list
-                moveText += Move(startFact, endFact, int(round(ownedFactCyb[i] / 4))) # Sets second move to factory
-            if len(neutFact) > 1:
-                neutFactProd.pop(neutFact.index(endFact)) # Removes previous factory production from list
-                neutFact.remove(endFact) # Removes previous factory from list
-                endFact = neutFact[neutFactProd.index(max(neutFactProd))] # Second highest factory from list
-                moveText += Move(startFact, endFact, int(round(ownedFactCyb[i] / 4))) # Sets second move to factory
-        print(moveText[:-1])
-    elif stillOpp:
-        moveText = ""
-        if turn == 10 or turn == 20:
-            startFact = ownedFact[random.randint(0, len(ownedFact) - 1)]
-            endFact = oppFact[oppFactProd.index(max(oppFactProd))]
-            moveText = "BOMB " + str(startFact) + " " + str(endFact) + ";"
-            
-        for i in range(len(ownedFact)):
-            startFact = ownedFact[i]
-            endFact = oppFact[oppFactProd.index(max(oppFactProd))] # Enemy factory with highest production
-            moveText += Move(startFact, endFact, int(round(ownedFactCyb[i] / 3))) # Sets one move to factory
-            if len(oppFact) > 1:
-                oppFactProd.pop(oppFact.index(endFact)) # Removes previous factory production from list
-                oppFact.remove(endFact) # Removes previous factory from list
-                endFact = oppFact[oppFactProd.index(max(oppFactProd))] # Second highest factory from list
-                moveText += Move(startFact, endFact, int(round(ownedFactCyb[i] / 3))) # Sets second move to factory
-        print(moveText[:-1])
-    else:
-        print("WAIT")'''
-    
-    
-''' WOOD 3 LEAGUE
-    if stillNeut: #If there are still neutral factories, send half of the factory with the most cyb to neutral factory
-        mostOwnCybFact = ownedFact[ownedFactCyb.index(max(ownedFactCyb))] # Finds most cyb, then position in list, then corresponding factory
-        startFact = mostOwnCybFact
-        
-        endFact = neutFact[neutFactProd.index(max(neutFactProd))] # Highest producing factories first
-        
-        
-        #Move(startFact, endFact, neutFactCyb[endFact - 1] + 1)
-        Move(startFact, endFact, int(round(max(ownedFactCyb)/2)))
-    elif stillOpp:
-        mostOwnCybFact = ownedFact[ownedFactCyb.index(max(ownedFactCyb))]
-        startFact = mostOwnCybFact
-        leastOppCybFact = oppFact[oppFactCyb.index(min(oppFactCyb))]
-        endFact = leastOppCybFact
-        
-        
-        Move(startFact, endFact, int(round(max(ownedFactCyb)/2)))
-    else:
-        print("WAIT")
-'''
-
-    # To debug: print("Debug messages...", file=sys.stderr)
-
-
+        moveText += Move(1, 2, str(cybsToSend))
+print(moveText)'''
